df33fadf2cebc2221df649d39f72f17201c3c9dc
ForExample <jyh6197@gmail.com>
2024-11-09 15:49:55 +0900

    feat: Project Initialize

---
+++ src/main/java/com/collectionuiback/CollectionUiBackApplication.java
@@ -0,0 +1,13 @@
+package com.collectionuiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionUiBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionUiBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/collectionuiback/CollectionUiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectionuiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionUiBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
