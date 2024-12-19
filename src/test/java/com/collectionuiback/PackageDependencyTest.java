28ff0946169eea94641da115bc4ad20ff42f00f8
ForExample <jyh6197@gmail.com>
2024-12-19 19:40:40 +0900

    Refactor and expand package dependency tests
    Reworked `PackageDependencyTest` to utilize JUnit 5 features, including `@BeforeAll`, `@DisplayName`, and `@Test`. Replaced static `@ArchTest` annotations with dynamic rule checks for clearer structure. Improved modularity and readability while aligning dependency rules with project architecture.

--- src/test/java/com/collectionuiback/PackageDependencyTest.java
+++ src/test/java/com/collectionuiback/PackageDependencyTest.java
@@ -1,42 +1,109 @@
 package com.collectionuiback;
 
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.junit.AnalyzeClasses;
-import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
-import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-@AnalyzeClasses(packagesOf = CollectionUiBackApplication.class)
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+@AnalyzeClasses
 public class PackageDependencyTest {
 
-    public static final String PACKAGE_BOILERPLATE = "..boilerplate..";
-    public static final String PACKAGE_MODULE = "..module..";
-    public static final String PACKAGE_INFRA = "..infra..";
-    public static final String PACKAGE_ACCOUNT = "..module.account..";
-    public static final String PACKAGE_EXCEPTION = "..module.exception..";
-    public static final String PACKAGE_OAUTH = "..module.oauth..";
+    public static final String ROOT_PACKAGE = "com.collectionuiback";
 
-    @ArchTest
-    ArchRule boilerplatePackageDependency = ArchRuleDefinition.noClasses()
-            .that().resideInAPackage(PACKAGE_BOILERPLATE)
-            .should().accessClassesThat().resideInAnyPackage(PACKAGE_MODULE, PACKAGE_INFRA);
+    public static final String PACKAGE_BOILERPLATE = ROOT_PACKAGE + ".boilerplate..";
+    public static final String PACKAGE_MODULE = ROOT_PACKAGE + ".module..";
+    public static final String PACKAGE_INFRA = ROOT_PACKAGE + ".infra..";
+    public static final String PACKAGE_ACCOUNT = ROOT_PACKAGE + ".module.account..";
+    public static final String PACKAGE_EXCEPTION = ROOT_PACKAGE + ".module.exception..";
+    public static final String PACKAGE_OAUTH = ROOT_PACKAGE + ".module.oauth..";
 
-    @ArchTest
-    ArchRule infraPackageDependency = ArchRuleDefinition.noClasses()
-            .that().resideInAPackage(PACKAGE_INFRA)
-            .should().accessClassesThat().resideInAnyPackage(PACKAGE_MODULE, PACKAGE_BOILERPLATE);
+    public static JavaClasses JAVA_CLASSES;
 
-    @ArchTest
-    ArchRule cycleCheck = SlicesRuleDefinition.slices().matching("com.collectionuiback.module.(*)..")
-            .should().beFreeOfCycles();
+    @BeforeAll
+    static void beforeAll() {
+        JAVA_CLASSES = new ClassFileImporter()
+                .importPackages(ROOT_PACKAGE);
+    }
 
-    @ArchTest
-    ArchRule exceptionPackageRule = ArchRuleDefinition.classes()
-            .that().resideInAPackage(PACKAGE_EXCEPTION)
-            .should().dependOnClassesThat().resideInAPackage(PACKAGE_BOILERPLATE);
+    @DisplayName("Boilerplate 패키지는 Module 패키지와 자기 자신에서만 참조가 가능하다.")
+    @Test
+    void boilerPlatePackageDependency() {
+        ArchRule boilerPlatePackageDependency = classes()
+                .that().resideInAPackage(PACKAGE_BOILERPLATE)
+                .should().onlyBeAccessed().byClassesThat().resideInAnyPackage(PACKAGE_MODULE, PACKAGE_BOILERPLATE);
 
-    @ArchTest
-    ArchRule oauthPackageRule = ArchRuleDefinition.classes()
-            .that().resideInAPackage(PACKAGE_OAUTH)
-            .should().onlyBeAccessed().byClassesThat().resideInAnyPackage(PACKAGE_BOILERPLATE, PACKAGE_OAUTH, PACKAGE_EXCEPTION, PACKAGE_ACCOUNT);
+        boilerPlatePackageDependency.check(JAVA_CLASSES);
+    }
+
+    @DisplayName("Boilerplate 패키지는 Infra, Module 패키지를 참조할 수 없다.")
+    @Test
+    void boilerPlatePackageDependency2() {
+        ArchRule boilerPlatePackageDependency = noClasses()
+                .that().resideInAPackage(PACKAGE_BOILERPLATE)
+                .should().accessClassesThat().resideInAnyPackage(PACKAGE_INFRA, PACKAGE_MODULE);
+
+        boilerPlatePackageDependency.check(JAVA_CLASSES);
+    }
+
+    @DisplayName("Infra 패키지는 Module 패키지와 자기 자신에서만 참조가 가능하다.")
+    @Test
+    void infraPackageDependency() {
+        ArchRule infraPackageDependency = classes()
+                .that().resideInAPackage(PACKAGE_INFRA)
+                .should().onlyBeAccessed().byClassesThat().resideInAnyPackage(PACKAGE_MODULE, PACKAGE_INFRA);
+
+        infraPackageDependency.check(JAVA_CLASSES);
+    }
+
+    @DisplayName("Infra 패키지는 Boilerplate, Module 패키지를 참조할 수 없다.")
+    @Test
+    void infraPackageDependency2() {
+        ArchRule infraPackageDependency = noClasses()
+                .that().resideInAPackage(PACKAGE_INFRA)
+                .should().accessClassesThat().resideInAnyPackage(PACKAGE_MODULE, PACKAGE_BOILERPLATE);
+
+        infraPackageDependency.check(JAVA_CLASSES);
+    }
+
+    @DisplayName("Module 패키지내의 순환참조는 발생하지 않는다.")
+    @Test
+    void modulePackageCycleCheck() {
+        ArchRule modulePackageCycleCheck = slices().matching("com.collectionuiback.module.(*)..")
+                .should().beFreeOfCycles();
+
+        modulePackageCycleCheck.check(JAVA_CLASSES);
+    }
+
+    @DisplayName("Exception 패키지는 Boilerplate 패키지만을 참조한다.")
+    @Test
+    void exceptionPackageDependency() {
+        ArchRule exceptionPackageDependency = classes()
+                .that().resideInAPackage(PACKAGE_EXCEPTION)
+                .should().dependOnClassesThat().resideInAPackage(PACKAGE_BOILERPLATE);
+
+        exceptionPackageDependency.check(JAVA_CLASSES);
+    }
+
+    @DisplayName("OAuth 패키지는 Boilerplate, Exception, Account 패키지만을 참조한다.")
+    @Test
+    void oauthPackageDependency() {
+        ArchRule oauthPackageDependency = classes()
+                .that().resideInAPackage(PACKAGE_OAUTH)
+                .should().dependOnClassesThat().resideInAnyPackage(
+                        PACKAGE_BOILERPLATE,
+                        PACKAGE_EXCEPTION,
+                        PACKAGE_ACCOUNT,
+                        "java..",
+                        "org.springframework.."
+                );
+
+        oauthPackageDependency.check(JAVA_CLASSES);
+    }
 }
